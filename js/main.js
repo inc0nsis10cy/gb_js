5f5295dc6b6802f19bc01a058c66bd5db6f4b3b1
met_ka <yekaterina.metelnikova@gmail.com>
2021-05-12 01:53:47 +0300

    corrections based on the in class solution demonstation

--- js/main.js
+++ js/main.js
@@ -32,12 +32,33 @@ const products = [
 ];
 
 //Функция для формирования верстки каждого товара
-const renderProduct = (photo, title = 'Some item', description = 'check', price = '0.0') => {
+// Изначальный вариант
+// const renderProduct = (photo, title = 'Some item', description = 'check', price = '0.0') => {
+//     return `<div class="product-item">
+//     <img class ="itemImg" src="img/${photo}" onerror = "this.onerror = null; this.src = 'img/img_unavailable.png'"> 
+//     <h3>${title}</h3>
+//     <div> ${description}</div>
+//     <div> Price: <span> $${price}</span> </div>
+//     <div> Quantity <input class = "inputQty" type="text" placeholder = "1"></div>
+//     <div class="itemDetails">
+//     <a href="#"> More info >> </a>
+//     <button class="buy-btn">Купить</button>
+//     </div>
+// </div>`
+// };
+
+// const renderPage = list => {
+//     const productsList = list.map(item => renderProduct(item.photo, item.title, item.description, item.price)).join('');
+//     document.querySelector('.products').innerHTML = productsList;
+// };
+
+//Функция для формирования верстки каждого товара (упрощенный)
+const renderProduct = (product) => {
     return `<div class="product-item">
-    <img class ="itemImg" src="img/${photo}" onerror = "this.onerror = null; this.src = 'img/img_unavailable.png'"> 
-    <h3>${title}</h3>
-    <div> ${description}</div>
-    <div> Price: <span> $${price}</span> </div>
+    <img class ="itemImg" src="img/${product.photo}" onerror = "this.onerror = null; this.src = 'img/img_unavailable.png'"> 
+    <h3>${product.title}</h3>
+    <div> ${product.description}</div>
+    <div> Price: <span> $${product.price}</span> </div>
     <div> Quantity <input class = "inputQty" type="text" placeholder = "1"></div>
     <div class="itemDetails">
     <a href="#"> More info >> </a>
@@ -45,13 +66,20 @@ const renderProduct = (photo, title = 'Some item', description = 'check', price 
     </div>
 </div>`
 };
+
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.photo, item.title, item.description, item.price)).join('');
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
 };
-
-
-
-
 renderPage(products);
+
+//Function to sum up all prices of items in products array
+function calcTotalPrice() {
+    let totalPrice = 0
+    for (let i = 0; i < products.length; i++) {
+        totalPrice += products[i].price;
+    }
+    totalPrice = totalPrice.toFixed(2);
+    return totalPrice;
+}
+console.log(calcTotalPrice());
+
